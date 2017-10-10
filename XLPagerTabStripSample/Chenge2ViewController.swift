88af1d4fd45aa5f24d427e62fda255757977b1ab
xhg-yamada <xhg-yamada@kddi.com>
2017-10-10 12:47:03 +0900

    ViewController追加

---
+++ XLPagerTabStripSample/Chenge2ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  Chenge2ViewController.swift
+//  XLPagerTabStripSample
+//
+//  Created by shogo.yamada on 2017/10/10.
+//  Copyright © 2017年 shogo.yamada. All rights reserved.
+//
+
+import UIKit
+
+class Chenge2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
